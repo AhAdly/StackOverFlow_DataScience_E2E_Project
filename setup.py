621c854343a47f20e6fbed8bd9202f64e63c7890
Ahmed Hassan <a7mad3dly@gmail.com>
2025-08-11 11:50:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Live illustration for cookie cutter ',
-    author='Deena gergis',
+    author='ahmed hassan',
     license='',
 )
